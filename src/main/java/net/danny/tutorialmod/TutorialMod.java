888d0f82519e25c20a9fac05546351beb42d0a84
ldanielgg <ldanielgg@hotmail.com>
2024-12-12 23:47:40 -0500

    test 1

--- src/main/java/net/danny/tutorialmod/TutorialMod.java
+++ src/main/java/net/danny/tutorialmod/TutorialMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Important Comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorial-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);

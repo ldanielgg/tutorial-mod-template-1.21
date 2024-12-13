5331f62670f1d0bdc72be1bfd2c954e308079e30
ldanielgg <ldanielgg@hotmail.com>
2024-12-13 00:27:39 -0500

    added pink garnet item

--- src/main/java/net/danny/tutorialmod/TutorialMod.java
+++ src/main/java/net/danny/tutorialmod/TutorialMod.java
@@ -2,16 +2,17 @@ package net.danny.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.danny.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // Important Comment
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		
+		ModItems.registerModItems();
 	}
 }
